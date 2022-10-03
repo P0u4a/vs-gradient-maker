ac788aae352ca303ee67736a0f699915faaaaa65
P0u4a <pouyamiria@gmail.com>
2022-10-03 16:52:03 +1030

    Create sidebar view

---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+</script>
+<!-- Here what we wanna do is use the aquire vscodeapi and send a messsage
+that is basically a command to launch gradient maker -->
+<button on:click={() => {
+
+}}>
+    Start Making
+</button>
