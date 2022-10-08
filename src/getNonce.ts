423110c8f303e84dfb6c3eb5d72a035beb294d78
P0u4a <pouyamiria@gmail.com>
2022-10-08 18:29:52 +1030

    Add type safety

--- src/getNonce.ts
+++ src/getNonce.ts
@@ -1,7 +1,7 @@
 // Can generate this with a better algorithm later
-export function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+export function getNonce(): string {
+	let text = "";
+	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	for (let i = 0; i < 32; i++) {
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	}
