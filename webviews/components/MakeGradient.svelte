37f28f2b58ef1e34a7edb258894d0e43213a88c3
P0u4a <pouyamiria@gmail.com>
2022-10-09 23:27:22 +1030

    Add radial gradient option

--- webviews/components/MakeGradient.svelte
+++ webviews/components/MakeGradient.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 
-    let directions = [
+    const linearDirections = [
         {angle: 0, logo: "⬆️"},
         {angle: 45, logo: "↗️"},
         {angle: 270, logo: "➡️"},
@@ -11,6 +11,18 @@
         {angle: 315, logo: "↖️"},
     ];
 
+    const radialPositions = [
+        {position: "center", logo: "🅾️"},
+        {position: "top", logo: "⬆️"},
+        {position: "right top", logo: "↗️"},
+        {position: "right", logo: "➡️"},
+        {position: "right bottom", logo: "↘️"},
+        {position: "bottom", logo: "⬇️"},
+        {position: "left bottom", logo: "↙️"},
+        {position: "left", logo: "⬅️"},
+        {position: "left top", logo: "↖️"}
+    ];
+
     let colors = {primaryColor: "#000000", secondaryColor: "#000000"};
 
     type SelectedDirection = {
@@ -18,40 +30,82 @@
         logo?: string;
     };
 
+    type SelectedPosition = {
+        position?: string;
+        logo?: string;
+    };
+
     let selectedAngle: SelectedDirection = {angle: 90};
+    let selectedPosition: SelectedPosition = {position: "center"}; 
 
     let animate = false;
+
+    let radial = false;
+
     let animationLength = 1;
 
 </script>
 
 <h1>Gradient Maker</h1>
 
-{#if animate}
-<div class="animateDisplay" 
-    style="background-image: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor}); --animationLength:{animationLength}s"
-/>
+{#if animate && !radial}
+    <div class="animateDisplay" 
+        style="background-image: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor}); --animationLength:{animationLength}s"
+    />
+{:else if animate && radial}
+    <div class="animateDisplay" 
+        style="background-image: radial-gradient(circle at {selectedPosition.position}, {colors.primaryColor}, {colors.secondaryColor}); --animationLength:{animationLength}s"
+    />
+{:else if !animate && radial}
+    <div class="noAnimateDisplay" 
+        style="background-image: radial-gradient(circle at {selectedPosition.position}, {colors.primaryColor}, {colors.secondaryColor})"
+    />
 {:else}
-<div class="noAnimateDisplay" 
-    style="background-image: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor})"
-/>
+    <div class="noAnimateDisplay" 
+        style="background-image: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor})"
+    />
 {/if}
 
 <form>
-    <b>Primary Color</b>
+    <b>Colors</b>
     <input type="color" bind:value={colors.primaryColor} />
-    <b>Secondary Color</b>
     <input type="color" bind:value={colors.secondaryColor} />
+    
+    <b>Gradient Type</b>
+    <label>
+        <input type=radio bind:group={radial} checked={true} value={false}>
+        Linear
+    </label>
+    
+    <label>
+        <input type=radio bind:group={radial} value={true}>
+        Radial
+    </label>
 
-    <b>Gradient Direction</b>
+    {#if radial}
+        <b>Gradient Position</b>
+    {:else}
+        <b>Gradient Direction</b>
+    {/if}
     <br />
-	<select bind:value={selectedAngle}>
-		{#each directions as direction}
-			<option value={direction}>
-				{direction.logo}
-			</option>
-		{/each}
-	</select>
+
+    {#if radial}
+        <select bind:value={selectedPosition}>
+            {#each radialPositions as position}
+                <option value={position}>
+                    {position.logo}
+                </option>
+            {/each}
+        </select>
+    {:else}
+        <select bind:value={selectedAngle}>
+            {#each linearDirections as direction}
+                <option value={direction}>
+                    {direction.logo}
+                </option>
+            {/each}
+        </select>
+    {/if}
     <br />
     
     <label>
@@ -59,31 +113,40 @@
         <b>Animate Gradient</b>
     </label>
     {#if animate}
-    <br />
-    <b>Animation Length</b>
-    <input type="range" min=1 max=59 bind:value={animationLength} />
+        <br />
+        <b>Animation Length</b>
+        <input type="range" min=1 max=59 bind:value={animationLength} />
     {/if}
 
-    <br />
     <b>CSS:</b>
     <div class="code">
         <code>
-            {#if animate}
-            background: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor});
-            <br />
-            animation: gradient {animationLength}s ease infinite;
-            <br />
-            @keyframes gradient &lbrace;
-                0%&lbrace;background-position:0% 50%&rbrace;
-                50%&lbrace;background-position:100% 50%&rbrace;
-                100%&lbrace;background-position:0% 50%&rbrace;
-            &rbrace;
+            {#if animate && !radial}
+                background: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor});
+                <br />
+                animation: gradient {animationLength}s ease infinite;
+                <br />
+                @keyframes gradient &lbrace;
+                    0%&lbrace;background-position:0% 50%&rbrace;
+                    50%&lbrace;background-position:100% 50%&rbrace;
+                    100%&lbrace;background-position:0% 50%&rbrace;
+                &rbrace;
+            {:else if animate && radial}
+                background: radial-gradient(circle at {selectedPosition.position}, {colors.primaryColor}, {colors.secondaryColor});
+                <br />
+                animation: gradient {animationLength}s ease infinite;
+                <br />
+                @keyframes gradient &lbrace;
+                    0%&lbrace;background-position:0% 50%&rbrace;
+                    50%&lbrace;background-position:100% 50%&rbrace;
+                    100%&lbrace;background-position:0% 50%&rbrace;
+            {:else if !animate && radial}
+                background: radial-gradient(circle at {selectedPosition.position}, {colors.primaryColor}, {colors.secondaryColor});
             {:else}
-            background: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor});
+                background: linear-gradient({selectedAngle.angle}deg, {colors.primaryColor}, {colors.secondaryColor});
             {/if}
         </code>
     </div>
-
 </form>
 
 <style>
