9196534d7aab07d526b16c15537ef3b4382953a1
P0u4a <pouyamiria@gmail.com>
2023-08-11 00:06:12 +0930

    Export gradient maker types

--- webviews/types.d.ts
+++ webviews/types.d.ts
@@ -1,20 +1,6 @@
 // Types for vscode api
 type VSCode = {
-    postMessage: ({ type: string, value: any }) => void;
+  postMessage: ({ type: string, value: any }) => void;
 };
 
 declare const vscode: VSCode;
-
-// Types for UI properties
-type ColorStop = {
-    color: string;
-    position: number;
-};
-
-type SelectedDirection = {
-    angle?: number;
-};
-
-type SelectedPosition = {
-    position?: string;
-};
